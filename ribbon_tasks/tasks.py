8ce8b7301c24ef54d94ccad7d2e7677eba9b7931
njf042 <njf042@gmail.com>
2025-03-05 18:34:20 -0800

    Add RaptorXSingle task configuration and container details

--- ribbon_tasks/tasks.py
+++ ribbon_tasks/tasks.py
@@ -6,14 +6,17 @@ import shutil
 import json
 
 class LigandMPNN(Task):
-    def __init__(self, output_dir, structure_list, num_designs=1, device='cpu', extra_args=None):
-        ''' Initialize a LigandMPNN task. 
+    def __init__(self, output_dir, structure_list, num_designs=1, device='cpu', extra_args=""):
+        """
+        Initialize a LigandMPNN task.
+
         Args:
-            output_dir (str): 
-                The directory to save the output files.
-            structure_list (list):
-                A list of pdb or cif files to use as input structures.
+            output_dir (str): The directory to save the output files.
+            structure_list (list): A list of pdb or cif files to use as input structures.
             num_designs (int): The number of designs to generate per input structure.
+            device (str): The device to run the task on. Default is 'cpu'.
+            extra_args (str): Additional arguments for the task. Default is an empty string.
+
         Returns:
             None
             Outputs the following directories:
@@ -21,8 +24,8 @@ class LigandMPNN(Task):
                     - backbones: The generated backbones
                     - packed: The packed structures, including sidechains
                     - sequences: The generated sequences as FASTA files. Multiple chains are separated by ':'. Each line is a different design.
-                    - seqs_split: The sequences split into separate FASTA files, one per design. Each line is a separate chain. 
-        '''
+                    - seqs_split: The sequences split into separate FASTA files, one per design. Each line is a different chain.
+        """
         # Initialize the Task class
         super().__init__(device=device, extra_args=extra_args)
 
@@ -35,7 +38,6 @@ class LigandMPNN(Task):
         self.num_designs = num_designs
 
     def run(self):
-        ''' Run the LigandMPNN task.'''
         
         ###### HELPER FUNCTIONS #######
         def split_ligandmpnn_fasta(fasta_file, split_output_dir):
@@ -94,10 +96,18 @@ class LigandMPNN(Task):
 
 class FastRelax(Task):
     def __init__(self, output_dir, pdb_input_file=None, pdb_input_dir=None, device='cpu'):
-        ''' 
-        TODO: fill out
-        Also, change to accept list of PDBs instead of input file/dir system
-        '''
+        """
+        Initialize a FastRelax task.
+
+        Args:
+            output_dir (str): The directory to save the output files.
+            pdb_input_file (str, optional): Path to a single PDB file. Default is None.
+            pdb_input_dir (str, optional): Path to a directory containing PDB files. Default is None.
+            device (str): The device to run the task on. Default is 'cpu'.
+
+        Raises:
+            ValueError: If neither pdb_input_file nor pdb_input_dir is specified.
+        """
          # Initialize the Task class
         super().__init__()
 
@@ -134,18 +144,17 @@ class FastRelax(Task):
         )
 
 class Chai1(Task):
-    def __init__(self, fasta_file, output_prefix, output_dir='.', smiles_string=None, num_ligands=1, device='gpu'):
-        ''' Run the Chai-1 task.
+    def __init__(self, fasta_file, output_dir='.', smiles_string=None, num_ligands=1, device='gpu'):
+        """
+        Initialize a Chai-1 task.
+
         Args:
-            fasta_file (str): 
-                The FASTA file containing the protein sequence (no ligand). Only runs 1 system, with multiple chains/ligands optional.
-            smiles_string (str): 
-                The SMILES string of the ligand.
-            output_prefix (str):
-                The prefix for the output files. The output will be named this, + '_pred.model_idx_X.cif'
-            output_dir (str):
-                The directory to save the output files.
-        '''
+            fasta_file (str): The FASTA file containing the protein sequence (no ligand).
+            output_dir (str): The directory to save the output files. Default is '.'.
+            smiles_string (str, optional): The SMILES string of the ligand. Default is None.
+            num_ligands (int): The number of ligands. Default is 1.
+            device (str): The device to run the task on. Default is 'gpu'.
+        """
         # Initialize the Task class
         super().__init__()
 
@@ -155,7 +164,6 @@ class Chai1(Task):
         # Task-specific variables
         self.fasta_file = fasta_file
         self.smiles_string = smiles_string
-        self.output_prefix = output_prefix
         self.output_dir = output_dir
         self.device = device
         self.num_ligands = num_ligands
@@ -171,26 +179,96 @@ class Chai1(Task):
             smiles_string=self.smiles_string,
             output_dir=str(self.output_dir),
             num_ligands=self.num_ligands,
-            output_prefix=self.output_prefix,
             device=self.device
+        )
+
+class RaptorXSingle(Task):
+    def __init__(self, fasta_file_or_dir, output_dir='.', param='RaptorX-Single-ESM1b.pt', device='gpu', extra_args=""):
+        """
+        Initialize a RaptorXSingle task.
+
+        Args:
+            fasta_file_or_dir (str): The FASTA file or directory containing multiple FASTA files.
+            output_dir (str): The directory to save the output files. Default is '.'.
+            param (str): The checkpoint to use. Default is 'RaptorX-Single-ESM1b.pt'.
+            device (str): The device to run the task on. Default is 'gpu'.
+            extra_args (str): Additional arguments for the task. Default is an empty string.
+
+        Raises:
+            ValueError: If an invalid param is specified.
+        """
+        
+        # Initialize the Task class
+        super().__init__()
+
+        # This Task name matches the name in the tasks.json file
+        self.task_name = "RaptorXSingle"
+
+        # Task-specific variables
+        self.fasta_file = fasta_file_or_dir
+        self.output_dir = output_dir
+        self.param = 'RaptorX-Single/params/'+param  #This is the directory where the params are stored
+        self.extra_args = extra_args
+        self.device = device
+
+        if device == 'gpu':
+            self.device_id = '0'
+        elif device == 'cpu':
+            self.device_id = '-1'
+        else:
+            self.device_id = str(device)
+            self.device = 'cpu' if device == '-1' else 'gpu'
+
+        # Check inputs:
+        valid_param_list = [
+            'RaptorX-Single-ESM1b.pt',
+            'RaptorX-Single-ESM1v.pt',
+            'RaptorX-Single-ProtTrans.pt',
+            'RaptorX-Single-ESM1b-ESM1v-ProtTrans.pt',
+            'RaptorX-Single-ESM1b-Ab.pt',
+            'RaptorX-Single-ESM1v-Ab.pt',
+            'RaptorX-Single-ProtTrans-Ab.pt',
+            'RaptorX-Single-ESM1b-ESM1v-ProtTrans-Ab.pt'
+        ]
+
+        if param not in valid_param_list:
+            raise ValueError(f'Invalid param: {param}. Must be one of {valid_param_list}')
+
+    def run(self):
+        # Make the directory:
+        self.output_dir = make_directory(self.output_dir)
+        
+        # "python RaptorX-Single/pred.py {fasta_file} RaptorX-Single/params/{param} --plm_param_dir RaptorX-Single/params/ --out_dir {output_dir} --device {device} {extra_args}",
+
+        # Run the task
+        self._run_task(
+            self.task_name,
+            fasta_file=self.fasta_file,
+            param=self.param,
+            output_dir=str(self.output_dir),
+            device=self.device,
+            device_id=self.device_id,
+            extra_args=self.extra_args
         )
 
 class CalculateDistance(Task):
     def __init__(self, pdb_file, chain1_id, res1_id, atom1_name,
                  chain2_id, res2_id, atom2_name, output_file, device='cpu'):
-        ''' Calculate the distance between two atoms in a PDB file.
+        """
+        Initialize a CalculateDistance task.
+        This calculates the distance between two atoms in a PDB file.
+
         Args:
-            pdb_file (str): Path to the PDB file
-            chain1_id (str): Chain ID of the first atom
-            res1_id (str): Residue ID of the first atom
-            atom1_name (str): Name of the first atom
-            chain2_id (str): Chain ID of the second atom
-            res2_id (str): Residue ID of the second atom
-            atom2_name (str): Name of the second atom
-            output_file (str): Path to the output file. Suffixed with '.dist'
-        Returns:
-            None
-        ''' 
+            pdb_file (str): Path to the PDB file.
+            chain1_id (str): Chain ID of the first atom.
+            res1_id (str): Residue ID of the first atom.
+            atom1_name (str): Name of the first atom.
+            chain2_id (str): Chain ID of the second atom.
+            res2_id (str): Residue ID of the second atom.
+            atom2_name (str): Name of the second atom.
+            output_file (str): Path to the output file. Suffixed with '.dist'.
+            device (str): The device to run the task on. Default is 'cpu'.
+        """
         # Initialize the Task class
         super().__init__()
 
@@ -227,15 +305,22 @@ class CalculateDistance(Task):
         )
 
 class CalculateSASA(Task):
-    def __init__(self, pdb_file, output_file, atom_1, atom_2, atom_3, atom_4, atom_5, atom_6, device='cpu'):
-        ''' Calculate the distance between two atoms in a PDB file.
+    def __init__(self, pdb_file, output_file, atom_1, device='cpu'):
+        """
+        Initialize a CalculateSASA task.
+        This calculates the Solvent Accessible Surface Area for a set of atoms in a PDB file.
+
         Args:
-            pdb_file (str):             Path to the PDB file
-            output_file (str):          Path to the output file. Suffixed with '.angle'
-            atom_1 - atom_6 (str):               Atom specifications, in format chain_id:res_id:atom_name.
-        Returns:
-            None
-        '''
+            pdb_file (str): Path to the PDB file.
+            output_file (str): Path to the output file. Suffixed with '.angle'.
+            atom_1 (str): Atom specification in format chain_id:res_id:atom_name.
+            device (str): The device to run the task on. Default is 'cpu'.
+
+        TODO:
+            - Implement the task script in ribbon/ribbon_tasks/task_scripts/calculate_sasa.py
+        """
+
+        raise NotImplementedError('This task is not yet implemented .')
         # Initialize the Task class
         super().__init__()
 
@@ -246,11 +331,6 @@ class CalculateSASA(Task):
         self.pdb_file = pdb_file
         self.output_file = output_file
         self.atom_1 = atom_1
-        self.atom_2 = atom_2
-        self.atom_3 = atom_3
-        self.atom_4 = atom_4
-        self.atom_5 = atom_5
-        self.atom_6 = atom_6
         self.device = device
 
     def run(self):
@@ -263,10 +343,39 @@ class CalculateSASA(Task):
             pdb_file=self.pdb_file,
             output_file=self.output_file,
             atom_1=self.atom_1,
-            atom_2=self.atom_2,
-            atom_3=self.atom_3,
-            atom_4=self.atom_4,
-            atom_5=self.atom_5,
-            atom_6=self.atom_6,
+            device=self.device
+        )
+
+
+class Custom(Task):
+    def __init__(self, command, container='Ribbon', device='cpu'):
+        """
+        Initialize a Custom task.
+        This allows the user to run a custom command in a specified container.
+        N.B. This allows the user to run arbitrary code; use with caution.
+
+        Args:
+            command (str): The command to run.
+            container (str): The container to run the command in. Default is 'Ribbon'.
+            device (str): The device to run the task on. Default is 'cpu'.
+        """
+        # Initialize the Task class
+        super().__init__()
+
+        # This Task name matches the name in the tasks.json file
+        self.task_name = "Custom"
+        
+        # Task-specific variables
+        self.command = command
+        self.container = container
+        self.device = device
+
+    def run(self):
+
+        # Run the task
+        self._run_task(
+            self.task_name,
+            command=self.command,
+            container_override=self.container,
             device=self.device
         )
